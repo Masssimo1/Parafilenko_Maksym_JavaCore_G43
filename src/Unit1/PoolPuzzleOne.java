18a3f5893203eee2242f532a6fcd4b9ced5b22b8
Masssimo1 <parafilenko@gmail.com>
2019-12-25 22:04:12 +0200

    Unit1 ready

---
+++ src/Unit1/PoolPuzzleOne.java
@@ -0,0 +1,50 @@
+package Unit1;
+
+public class PoolPuzzleOne {
+
+    public static void main(String[] args) {
+        // x < 1
+        // x > 0 --
+        // x > 1
+        // x > 3 --
+        // x < 4
+
+        // x = x + 1;
+        // x = x + 2; --
+        // x = x - 2;
+        // x = x - 1; --
+
+        // System.out.print("a "); --  /** В книге не верный ответ, так как в условии стринга с пробелом!!!! */
+        // System.out.print(" ");
+        // System.out.print("n ");
+        // System.out.print("an"); --
+
+        // System.out.print("noise");  --
+        // System.out.print("noys");
+        // System.out.print("oise");
+        // System.out.print(" oyster");
+        // System.out.print("annoys"); --
+
+
+        int x = 0;
+
+        while ( x < 4 ) {
+            if ( x  < 1 ){
+                System.out.print("a ");
+                System.out.print("noise");
+                x = x -1;
+            }
+            if ( x > 0 ) {
+                System.out.print("an");
+            }
+            if ( x == 1) {
+                System.out.print("noys");
+            }
+            x = x + 2;
+            if ( x > 3 ) {
+                System.out.print(" oyster");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ src/Unit1/Task2/C.java
@@ -0,0 +1,13 @@
+package Unit1.Task2;
+
+public class C {
+    public static void main(String[] args) { // не был добавлен метод
+        int x = 5;
+        while ( x > 1 ) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("маленький икс");
+            }
+        }
+    }
+}
